62aff8a3343a22da4eab30ac009ebd02673039c7
Hawraa7 <hawraahijazi1996@gmail.com>
2025-03-05 15:32:46 -1000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path("event/<int:event_id>/", event_detail, name="event_detail"),
     path("post/<slug:slug>/", post_detail, name="post_detail"),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Event, Post  # Ensure Post is imported
+from django.http import HttpResponseRedirect
+from .models import Event, Post, Comment
 from .forms import CommentForm
 
 class PostList(generic.ListView):
    model = Event
    template_name = "index.html"
    paginate_by = 12
-
 
 def event_detail(request, event_id):
    event = get_object_or_404(Event, id=event_id)
@@ -19,6 +19,7 @@ def post_detail(request, slug):
    post = get_object_or_404(queryset, slug=slug)
    comments = post.comments.all().order_by("-created_on")
    comment_count = post.comments.filter(approved=True).count()
+
    if request.method == "POST":
       comment_form = CommentForm(data=request.POST)
       if comment_form.is_valid():
@@ -29,7 +30,7 @@ def post_detail(request, slug):
          messages.add_message(
          request, messages.SUCCESS,
          'Comment submitted and awaiting approval'
-    )
+   )
 
    comment_form = CommentForm()
 
@@ -41,5 +42,37 @@ def post_detail(request, slug):
          "comments": comments,
          "comment_count": comment_count,
          "comment_form": comment_form,
-    },
-    )
+      },
+   )
+
+def comment_edit(request, slug, comment_id):
+   if request.method == "POST" :
+      queryset = Post.objects.filter(status=1)
+      post = get_object_or_404(queryset, slug=slug)
+      comment = get_object_or_404(Comment, pk=comment_id)
+      comment_form = CommentForm(data=request.POST, instance=comment)
+
+      if comment_form.is_valid() and comment.author == request.user:
+         comment = comment_form.save(commit=False)
+         comment.post = post
+         comment.approved = False
+         comment.save()
+         messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+      else:
+         messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+   return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+   queryset = Post.objects.filter(status=1)
+   post = get_object_or_404(queryset, slug=slug)
+   comment = get_object_or_404(Comment, pk=comment_id)
+
+   if comment.author == request.user:
+      comment.delete()
+      messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+   else:
+      messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+   return HttpResponseRedirect(reverse('post_detail', args=[slug]))
